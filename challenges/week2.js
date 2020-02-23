6306248555fd8751bca7f7ef497ea0f2dbf3ff41
Hannah <hannahbeardmore1@hotmail.co.uk>
2020-02-23 17:08:42 +0000

    week 2 homework - all tests pass

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,27 +1,49 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if (person.city === "Manchester") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  let bus = people / 40
+  let numOfBus = Math.ceil(bus)
+  return numOfBus
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  let count = 0
+  arr.forEach(animal => {
+    if (animal === "sheep") {
+      count++
+    }
+  })
+  return count
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  
+  if (person.address.city != "Manchester") {
+    return false
+  }
+
+  if (person.address.postCode.startsWith("M")) {
+    return true
+  } else {
+    return false
+  }
 }
+
 
 module.exports = {
   getFillings,
